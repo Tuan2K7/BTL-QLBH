624186d6368d4d83c49ead551ce95d2a0d6f84b0
TranThong0908 <thongtran09082007@gmail.com>
2026-06-04 16:05:31 +0700

    Update menu.c

--- Code/menu.c
+++ Code/menu.c
@@ -2,17 +2,17 @@
 
 void khoi_tao_menu(void) {
      menu[0].stt = 1; strcpy(menu[0].ten, "Mi Kim Chi Thap Cam");   
-                                                                    menu[0].don_gia = 65000; //1
+     menu[0].don_gia = 65000; //1
      menu[1].stt = 2; strcpy(menu[1].ten, "Mi Kim Chi Dui Ga");     
-                                                                    menu[1].don_gia = 55000;   //2
+     menu[1].don_gia = 55000; //2
      menu[2].stt = 3; strcpy(menu[2].ten, "Mi Kim Chi Bo");         
-                                                                    menu[2].don_gia = 55000; //3
+     menu[2].don_gia = 55000; //3
      menu[3].stt = 4; strcpy(menu[3].ten, "Mi Kim Chi Bach Tuoc");  
-                                                                    menu[3].don_gia = 55000; // 4
+     menu[3].don_gia = 55000; //4
      menu[4].stt = 5; strcpy(menu[4].ten, "Mi Kim Chi Xuc Xich");    
-                                                                    menu[4].don_gia = 45000;//5
+     menu[4].don_gia = 45000; //5
      menu[5].stt = 6; strcpy(menu[5].ten, "Mi Kim Chi Hai San");    
-                                                                    menu[5].don_gia = 55000; //6
+     menu[5].don_gia = 55000; //6
 }
 
 void hien_thi_menu(void) {
